5df57f7822a01ce5e4bf53ded2b058b4f83a6608
Mateusz Piekny <m.piekny97@gmail.com>
2022-03-07 13:54:08 +0100

    Add initList() function

--- task1_list/list.cpp
+++ task1_list/list.cpp
@@ -18,89 +18,24 @@ List* initList(void)
 
 void deinitList(List* pRoot)
 {
-	List* pCurrent = pRoot;
-	List* pPrevious;
-
-	if (pRoot == NULL) return;
-
-	while (pRoot->pNext != NULL)
-	{
-		while (pCurrent->pNext != NULL)
-		{
-			pPrevious = pCurrent;
-			pCurrent = pCurrent->pNext;
-		}
-		free(pCurrent);
-		pCurrent = NULL;
-		pPrevious->pNext = NULL;
-		pCurrent = pRoot;
-	}
-
 	free(pRoot);
 	pRoot = NULL;
 }
 
-void addCar(List* pRoot, const char* plateNum)
+void addCar(List* pRoot, char* plateNum)
 {
-	List* pNext = (List*)malloc(sizeof(*pRoot));
-	List* pCurrent = pRoot;
 
-	if (pNext == NULL)
-	{
-		std::cout << "Allocation error";
-		return;
-	}
-
-	while (pCurrent->pNext != NULL) pCurrent = pCurrent->pNext;
-
-	pNext->entryTime = time(NULL);
-	pNext->pPlateNum = const_cast<char*>(plateNum);
-	pNext->pNext = NULL;
-	pCurrent->pNext = pNext;
 }
 
-void removeCar(List* pRoot, const char* plateNum)
+void removeCar(List* pRoot, char* plateNum)
 {
-	List* pCurrent = pRoot;
-	List* pPrevious;
 
-	while (pCurrent->pNext != NULL)
-	{
-		pPrevious = pCurrent;
-		pCurrent = pCurrent->pNext;
-
-		if (std::strcmp(pCurrent->pPlateNum, plateNum))
-		{
-			pPrevious->pNext = pCurrent->pNext;
-			free(pCurrent);
-			pCurrent = NULL;
-			break;
-		}
-	}
 }
+void printAll(List* pRoot) {
 
-void printAll(List* pRoot) 
-{
-	List* pCurrent = pRoot->pNext;
-	time_t time;
-
-	while (pCurrent != NULL)
-	{
-		time = pCurrent->entryTime;
-		std::cout << "[" << strtok(ctime(&time),"\n") << "] Wjechal samochod o numerze rejestracyjnym " << pCurrent->pPlateNum << std::endl;
-		pCurrent = pCurrent->pNext;
-	}
 }
 
 int numberOfElements(List* pRoot)
 {
-	List* pCurrent = pRoot->pNext;
-	int count = 0;
-
-	while (pCurrent != NULL)
-	{
-		count++;
-		pCurrent = pCurrent->pNext;
-	}
-	return count;
+	return 0;
 }
--- task1_list/main.cpp
+++ task1_list/main.cpp
@@ -9,23 +9,6 @@ int main()
 	List* pFirstFloor = initList();
 	List* pSecFloor = initList();
 	
-	addCar(pFirstFloor, "EA27WB");
-	//_sleep(2000);
-	addCar(pFirstFloor, "EB34WB");
-	//_sleep(1000);
-	addCar(pFirstFloor, "EC22WB");
-	addCar(pFirstFloor, "ED22WB");
-	addCar(pFirstFloor, "EE22WB");
-	
-	printAll(pFirstFloor);
-	std::cout <<"Liczba aut: " << numberOfElements(pFirstFloor) << std::endl << std::endl;
-
-	removeCar(pFirstFloor, "EB34WB");
-	removeCar(pFirstFloor, "EE22WB");
-
-	printAll(pFirstFloor);
-	std::cout << "Liczba aut: " << numberOfElements(pFirstFloor) << std::endl << std::endl;
-
 	deinitList(pFirstFloor);
 	deinitList(pSecFloor);
 }
