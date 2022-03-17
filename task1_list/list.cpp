0e5fd9fc05a7e6c14313d56f708f2863e7dd8ebc
OlexTix <olexix@gmail.com>
2022-03-17 14:36:34 +0100

    Update list.cpp

--- task1_list/list.cpp
+++ task1_list/list.cpp
@@ -1,36 +1,15 @@
 #include "pch.h"
 #include "list.h"
-#include "iostream"
 
-using namespace std;
+
 
 List* initList(void)
 {
-        List * pRoot = (List*)malloc(sizeof(*pRoot));
+    List* pRoot = (List*)malloc(sizeof(*pRoot));
     pRoot->pPlateNum = (char*)malloc(strlen("Root") + 1u);
     strcpy(pRoot->pPlateNum, "Root");
     time(&(pRoot->entryTime));
     pRoot->pNext = NULL;
-
-    return pRoot;
-
-        List * pRoot = (List*)malloc(sizeof(*pRoot));
-
-    if (pRoot == NULL)
-    {
-        cout << "Memory allocation error";
-    }
-    else if (pRoot != NULL)
-    {
-        pRoot->pNext = NULL;
-        cout << "Memory allocation done successfully" << endl;
-    }
-
-    else
-    {
-        cout << "pNext not NULL. Error" << endl;
-
-    }
 
     return pRoot;
 }
@@ -106,14 +85,12 @@ void removeCar(List* pRoot, char* plateNum)
     }
 
 }
-
-    void printAll(List * pRoot)
-    
+void printAll(List* pRoot)
 {
 
 }
 
-int numberOfElements(List * pRoot)
+int numberOfElements(List* pRoot)
 {
     return 0;
 }
