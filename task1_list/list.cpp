cc7b4772d39389ef87dc30d45af1176c870bf3cc
anaastastasiia <85504422+anaastastasiia@users.noreply.github.com>
2022-03-18 18:36:01 +0100

    added changes to List* initList(void)

--- task1_list/list.cpp
+++ task1_list/list.cpp
@@ -1,107 +1,43 @@
 #include "pch.h"
 #include "list.h"
 
-
-
 List* initList(void)
 {
-    List* pRoot = (List*)malloc(sizeof(*pRoot));
-    pRoot->pPlateNum = (char*)malloc(strlen("Root") + 1u);
-    strcpy(pRoot->pPlateNum, "Root");
-    time(&(pRoot->entryTime));
-    pRoot->pNext = NULL;
+	List* pRoot = (List*)malloc(sizeof(*pRoot));
 
-    return pRoot;
+	if (pRoot != NULL)
+	{
+		pRoot->pNext = NULL;
+		std::cout << "Alokacja pamieci zakonczona pomyslnie :)";
+	}
+	else
+	{
+		std::cout << "Blad przydzialu pamieci :(";
+	}
+
+	return pRoot;
 }
 
 void deinitList(List* pRoot)
 {
-    free(pRoot->pPlateNum);
-    free(pRoot);
+	free(pRoot);
+	pRoot = NULL;
 }
 
 void addCar(List* pRoot, char* plateNum)
-	
-   if (NULL!=pPlateNum)
-   {
-       pPlateNum ->pNext = NULL
-       std:: cout << "pPlateNum, not empty";
-   }
-
 {
-    List* pLastElement = pRoot;
-    List* pNewElement = NULL;
 
-    while (pLastElement->pNext != NULL)
-    {
-        pLastElement = pLastElement->pNext;
-    }
-
-    pNewElement = (List*)malloc(sizeof(*pNewElement));
-    pNewElement->pPlateNum = (char*)malloc(strlen(plateNum) + 1u);
-    strcpy(pNewElement->pPlateNum, plateNum);
-    time(&(pNewElement->entryTime));
-    pNewElement->pNext = NULL;
-    pLastElement->pNext = pNewElement;
 }
 
 void removeCar(List* pRoot, char* plateNum)
 {
-    bool elementFound = false;
-    List* pIterElementBefre = NULL;
-    List* pIterElement = pRoot;
-    List* pIterElementAfter = pRoot->pNext;
-
-    //search until the end of the list or till the element is found
-    while ((NULL != pIterElement) && (false == elementFound))
-    {
-        //Check if we are looking for pIterElement 
-        if (0 != strstr(pIterElement->pPlateNum, plateNum))
-        {
-            elementFound = true; //set the flag
-        }
-        else //element is still not found
-        {
-            //if there is next element move to it
-            if (NULL != pIterElement->pNext)
-            {
-                pIterElementBefre = pIterElement;
-                if (NULL != pIterElement->pNext)
-                    pIterElementAfter = (pIterElement->pNext->pNext);
-                else
-                    pIterElementAfter = NULL;
-                pIterElement = (pIterElement->pNext);
-            }
-        }
-    }
-    //if the element has been found
-    if (true == elementFound)
-    {
-        //if element is the last in the list
-        if (NULL == pIterElementAfter)
-        {
-            pIterElementBefre->pNext = NULL;
-        }
-        else //our element is in the middle in the list
-        {
-            pIterElementBefre->pNext = pIterElementAfter;
-        }
-        //remove element from the memory
-        free(pIterElement->pPlateNum);
-        free(pIterElement);
-    }
 
 }
-
-void printAll(List* pRoot) 
-
-{
+void printAll(List* pRoot) {
 
 }
 
 int numberOfElements(List* pRoot)
 {
-
 	return 0;
 }
-
