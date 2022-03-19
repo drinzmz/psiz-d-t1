0d2fe619fd24739210d7f84b4e8b4bf3d9bb2a67
KovalchukYuliia <101018578+KovalchukYuliia@users.noreply.github.com>
2022-03-19 11:47:16 +0100

    comments added

--- task1_list/list.h
+++ task1_list/list.h
@@ -9,10 +9,10 @@ typedef struct List {
 } List;
 
 List*initList(void);
-void deinitList(List* pRoot);
-void addCar(List* pRoot, char* plateNum);
-void removeCar(List* pRoot, char* plateNum);
-void printAll(List* pRoot);
-int numberOfElements(List* pRoot);
+void deinitList(List* pRoot); //empty the list to execute the following functions
+void addCar(List* pRoot, char* plateNum);// we add cars to the list of parking lot cars
+void removeCar(List* pRoot, char* plateNum);// removes a car that has left the parking lot
+void printAll(List* pRoot); // show all cars in the parking lot
+int numberOfElements(List* pRoot);// show number of cars
 
 #endif // !LIST_H
