80d12d252fcad9ef89980f0103e9243cb5be3bac
mosinsky <97613980+mosinsky@users.noreply.github.com>
2022-06-28 16:08:22 +0200

    Update list.cpp

--- task1_list/list.cpp
+++ task1_list/list.cpp
@@ -45,7 +45,7 @@ void deinitList(List* pRoot)
 
 }
 
-void addCar(List* pRoot, char* plateNum)
+void addCar(List* pRoot, char* plateNum) //adding car to list
 {
     List* pLastElement = pRoot;
     List* pNewElement = NULL;
